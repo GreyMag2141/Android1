788a3333f863f396fe38698a1c8ede0694b00068
GreyMag2141 <grey2141@yandex.ru>
2017-12-16 13:27:16 +1000

    Градусы

--- app/src/main/java/ru/myfirstapp/myapplication/MainActivity.java
+++ app/src/main/java/ru/myfirstapp/myapplication/MainActivity.java
@@ -1,19 +1,20 @@
 package ru.myfirstapp.myapplication;
 
-import android.graphics.Color;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText input1;
-    EditText input2;
+//    EditText input2;
     Button button1;
     Button button2;
+    Button button3;
+    Button button4;
     TextView output;
     private boolean p;
 
@@ -22,33 +23,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        input1 =  findViewById(R.id.input1);
-        input2 =  findViewById(R.id.input2);
-        output =  findViewById(R.id.output);
-        button1 =  findViewById(R.id.button1);
-        button2 =  findViewById(R.id.button2);
+        input1 = findViewById(R.id.input1);
+//        input2 = findViewById(R.id.input2);
+        output = findViewById(R.id.output);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
-
-
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        String s1 = input1.getText().toString();
+//        String s2 = input2.getText().toString();
+        double x1 = Integer.parseInt(s1);
+//        int x2 = Integer.parseInt(s2);
         switch (v.getId()){
             case R.id.button1:
-        String s1 = input1.getText().toString();
-        String s2 = input2.getText().toString();
-        int x1 = Integer.parseInt(s1);
-        int x2 = Integer.parseInt(s2);
-        output.setText(String.valueOf(x1+x2));
+        output.setText(String.valueOf(x1*1.8+32));
                 break;
             case R.id.button2:
-                String s3 = input1.getText().toString();
-                String s4 = input2.getText().toString();
-                int y1 = Integer.parseInt(s3);
-                int y2 = Integer.parseInt(s4);
-                output.setText(String.valueOf(y1-y2));
+               output.setText(String.valueOf(x1+273.15));
+               break;
+            case R.id.button3:
+                output.setText(String.valueOf(x1*0.8));
+                break;
+            case R.id.button4:
+                output.setText(String.valueOf((100-x1)*1.5));
                 break;
         }
     }
